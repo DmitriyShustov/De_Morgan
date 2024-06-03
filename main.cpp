19cd352f66e3c89ccd8b5d765df6fe7e7292a286
shustov <shustov.dm.s@gmail.com>
2024-06-03 20:09:14 +0300

    Manual testing of parse_tree in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <QCoreApplication>
+#include "tree.h"
+#include "to_tree.h"
 
-int main(int argc, char *argv[])
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    QString str = "y x y  & ! forall ! ";
+    QVector<error> array_of_errors;
 
-    return a.exec();
+    tree* tree_root = parse_tree(str, array_of_errors);
+
+    for (int i = 0; i < array_of_errors.length(); ++i) {
+        qDebug() << array_of_errors[i].error_message;
+    }
+
+    return 0;
 }
