c851cf54d18d7c96ff13c049855ca549ed2a82b4
shustov <shustov.dm.s@gmail.com>
2024-06-19 17:15:39 +0300

    Add implementation the main computational function and auxillary functions for it

--- tree.cpp
+++ tree.cpp
@@ -162,3 +162,98 @@ type_node convert_string_to_type_node(QString string) {
         return VAR;
     }
 }
+
+void perform_logical_expression_transformations(tree* parent, tnode *node) {
+    // если текущий узел дерева пустой, то закончить алгоритм
+    if (node == nullptr) {
+
+        return;
+    }
+
+    // определить тип операции, которую необходимо совершить (если нужно)
+    transformation current_transformation = determine_type_of_transformation(node);
+    // если нужно совершить преобразование, то выполнить его
+    if ( current_transformation != NO_TRANSFORMATION ) {
+        perform_definite_transformation(parent, node, current_transformation);
+    }
+
+    perform_logical_expression_transformations(parent, node->left); //Рекурсивная функция для левого поддерева
+    perform_logical_expression_transformations(parent, node->right); //Рекурсивная функция для правого поддерева
+}
+
+transformation determine_type_of_transformation(tnode* node) {
+    // если встретили узел "отрицание"
+    if(node->type == NOT) {
+        // если встретили подходящую операци для раскрытия, то вернуть тип операции, которую нужно будет выполнить
+        switch (node->left->type) {
+        case NOT:
+
+            return DOUBLE_NEG_DELETE;
+        case CON:
+
+            return CON_OPENING;
+        case DIS:
+
+            return DIS_OPENING;
+        case EXIST:
+
+            return EXIST_OPENING;
+        case FORALL:
+
+            return  FORALL_OPENING;
+        default:
+
+            return NO_TRANSFORMATION;
+        }
+    }
+    // если не встретила нужная операция, операцию выполнять не нужно
+    else {
+
+        return NO_TRANSFORMATION;
+    }
+}
+
+void perform_definite_transformation(tree* parent, tnode *node, transformation type) {
+    switch (type) {
+    case DOUBLE_NEG_DELETE:
+        remove_double_negatives(parent, node);
+
+        return;
+    case CON_OPENING:
+        perform_dis_or_con_opening(parent, node, type);
+
+        return;
+    case DIS_OPENING:
+        perform_dis_or_con_opening(parent, node, type);
+
+        return;
+    case EXIST_OPENING:
+        perform_exist_or_forall_opening(parent, node, type);
+
+        return;
+    case FORALL_OPENING:
+        perform_exist_or_forall_opening(parent, node, type);
+
+        return;
+    default:
+
+        return;
+    };
+
+    return;
+}
+
+// выполнить удаление двойного отрицая в дереве
+void remove_double_negatives(tree* parent, tnode* node){
+    ;
+}
+
+// выполнить раскрытие дизъюнкции или конъюнкциипо закону Де Мограна
+void perform_dis_or_con_opening(tree* parent, tnode* node, transformation type){
+    ;
+}
+
+// выполнить раскрытие квантора существования или квантора всеобщности
+void perform_exist_or_forall_opening(tree* parent, tnode* node, transformation type){
+    ;
+}
