76e202e73d9e5de4f23d2db2b5d9ca104ad9168d
shustov <shustov.dm.s@gmail.com>
2024-06-20 00:46:35 +0300

    Implement interaction with files

--- main.cpp
+++ main.cpp
@@ -3,31 +3,69 @@
 #include "to_tree.h"
 #include "to_dot.h"
 #include "QtDebug"
+#include "QFile"
+#include "QTextStream"
+#include "string"
 
 #include <iostream>
 
-QString error_to_str(const error& e) {
-    std::stringstream a;
-    a << e.error_type << ":" << e.error_message.toStdString();
-    QString result(a.str().c_str());
-    return result;
-}
-
-int main()
+int main(int argc, char* argv[])
 {
-    // std::cout << error_to_str({"123", OPERATION_HAS_TOO_FEW_OPERANDS}).toStdString() << std::endl;
-    // return 0;
+    argc = 3;
+    argv[1] = "E:\\de_morgan_input.txt";
+    argv[2] = "E:";
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << std::endl;
+    }
 
-    QString str = "x y ! forall";
+    QFile file(argv[1]);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        std::cout << "Не удалось открыть файл для чтения" << std::endl;
+
+        return 0;
+    }
+
+    QTextStream in(&file);
+    QString input_string = in.readLine();
+
+    file.close();
+
+    // QString str = "x y ! forall";
     QVector<error> array_of_errors;
 
-    tree* tree_root = parse_tree(str, array_of_errors);
+    tree* tree_root = parse_tree(input_string, array_of_errors);
 
     for (int i = 0; i < array_of_errors.length(); ++i) {
         qDebug() << array_of_errors[i].error_message;
     }
 
-    QString dot_string = convert_tree_to_dot(tree_root);
+    if (array_of_errors.length() == 0) {
+        QFile original_file(QString::fromUtf8(argv[2]) + QString("\\original_tree.txt"));
+        if (!original_file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            qDebug() << "Не удалось открыть файл для записи";
+
+            return 0;
+        }
+
+        QTextStream out(&original_file);
+        out << convert_tree_to_dot(tree_root) << "\n";
+
+        original_file.close();
+
+        perform_logical_expression_transformations(tree_root, tree_root->top);
+
+        QFile transformed_file(QString::fromUtf8(argv[2]) + QString("\\transformed_tree.txt"));
+        if (!transformed_file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            qDebug() << "Не удалось открыть файл для записи";
+
+            return 0;
+        }
+
+        QTextStream out1(&transformed_file);
+        out1 << convert_tree_to_dot(tree_root) << "\n";
+
+        transformed_file.close();
+    }
 
     return 0;
 }
